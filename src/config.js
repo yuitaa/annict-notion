9bc28ab162a4cb0e5dbfa4bf1ad114b4637c3245
yuitaa <95457974+yuitaa@users.noreply.github.com>
2025-07-03 14:22:28 +0900

    dotenvを使用して環境変数を設定し、config.jsにANNICT_TOKEN、NOTION_TOKEN、NOTION_DB_IDをエクスポート

---
+++ src/config.js
@@ -0,0 +1,6 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
+export const ANNICT_TOKEN = process.env.ANNICT_TOKEN;
+export const NOTION_TOKEN = process.env.NOTION_TOKEN;
+export const NOTION_DB_ID = process.env.NOTION_DB_ID;
